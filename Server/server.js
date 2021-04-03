7d194ec484e6f5de7795e9262726d9b96b45ec79
AlexandruGiupana <73744919+AlexandruGiupana@users.noreply.github.com>
2021-04-03 14:06:40 +0300

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/',postRoutes);
 
-const connectionString = 'mongodb+srv://alex:1@clusterproiect.d25lj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.set("view engine", "ejs");
+app.set("views", "../views");
+
+const connectionString = 'mongodb+srv://alex:1@clusterproiect.d25lj.mongodb.net/JobPlatform?retryWrites=true&w=majority';
 const PORT=process.env.PORT||9090;
 mongoose.connect(connectionString,{useNewUrlParser:true,useUnifiedTopology:true})
         .then(()=> app.listen(PORT,()=>console.log(`Server running on port : ${PORT}`)))
