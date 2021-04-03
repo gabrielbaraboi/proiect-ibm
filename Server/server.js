d59c121509bb54a3ca52b4e9f52ff0554dccca1a
Rosca Paul <paul.rosca00@e-uvt.ro>
2021-04-04 02:03:14 +0300

    Model de site

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import postRoutes from './routes/posts.js';
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use('/',postRoutes);
+app.use(cors());
+app.use('/posts',postRoutes);
 
 
 const connectionString = 'mongodb+srv://alex:1@clusterproiect.d25lj.mongodb.net/JobPlatform?retryWrites=true&w=majority';
