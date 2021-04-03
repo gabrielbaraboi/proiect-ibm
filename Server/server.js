ae1ae23780f67672e79e8e43bc6218dadc12e9b7
AlexandruGiupana <73744919+AlexandruGiupana@users.noreply.github.com>
2021-04-03 10:09:21 +0300

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,70 +1,73 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose"
 
 
-app = express();
+
+
+import Post from './dbModels/PostModel.js';
+// import User from './dbModels/UserModel.js';
+
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-const connectionString = 'mongodb+srv://alex:1@clusterproiect.d25lj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-        console.log('Connected to Database');
+app.set("view engine", "ejs");
+app.set("views", "../views");
 
-        app.set("view engine", "ejs");
-        app.set("views", path.join(__dirname, "views"));
-        
-        const db = client.db("JobPlatform");
-        
-        app.get('/', (req, res) => {
-            db.collection('Posts').aggregate([   //join la tabele pentru a avea si numele firmei
-            
-                { $match : { type : "offer" }},
-                { $lookup:
-                    {
-                        from: 'Users',
-                        localField: 'createdBy',
-                        foreignField: '_id',
-                        as: 'companie'
-                    }
-                }
-            ]).toArray()
-                .then(results => {
-                    // console.log(results[1]['title']);
-                    res.render("index", { posturi: results });
-                })
-                .catch(error => console.error(error));
-        });
+const connectionString = 'mongodb+srv://alex:1@clusterproiect.d25lj.mongodb.net/JobPlatform?retryWrites=true&w=majority';
 
-        app.get('/detaliiPost', (req, res) => { //transmitem informatile anuntului dorit catre pagina detalii post
-            
-            db.collection('Posts').aggregate([
-            
-                { $match : { _id: new ObjectID(req.query.id) }},
-                { $lookup:
-                    {
-                        from: 'Users',
-                        localField: 'createdBy',
-                        foreignField: '_id',
-                        as: 'companie'
-                    }
-                }
-            ]).toArray()
-            .then(results => {
-                // console.log(results);
-                res.render("detaliiPost", { detalii: results[0]});
-            })
-            .catch(error => console.error(error));
-        });
-  })
-  .catch(error => console.error(error));
+mongoose
+    .connect(connectionString, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .catch(error => console.error(error));
+
+
+app.get('/', (req, res) => {
+    Post.collection.aggregate([   //join la tabele pentru a avea si numele firmei
+    
+        { $match : { type : "offer" }},
+        { "$addFields": {"createdBy_id" :  { "$toObjectId": "$createdBy" }}},
+        { $lookup:
+            {
+                from: 'Users',
+                localField: 'createdBy_id',
+                foreignField: '_id',
+                as: 'companie'
+            }
+        }
+    ]).toArray()
+        .then(results => {
+            res.render("index", { posturi: results }); //trimitem rezultatul in index.ejs prin varibila posturi
+        })
+        .catch(error => console.error(error));
+});
+
+app.get('/detaliiPost', (req, res) => { //transmitem informatile anuntului dorit catre pagina detalii post
+    
+    Post.collection.aggregate([ //join la tabele pentru a avea si numele firmei
+        
+        { $match : { _id: mongoose.Types.ObjectId(req.query.id) }},
+        { "$addFields": {"createdBy_id" :  { "$toObjectId": "$createdBy" }}},
+        { $lookup:
+            {
+                from: 'Users',
+                localField: 'createdBy_id',
+                foreignField: '_id',
+                as: 'companie'
+            }
+        }
+        ]).toArray()
+        .then(results => {
+            res.render("detaliiPost", { detalii: results[0]}); //trimitem rezultatul in detaliiPost.ejs prin varibila posturi
+        })
+        .catch(error => console.error(error));
+    });
 
 app.listen(9999, () => {    
     console.log('listening on 9999');
 });
-
