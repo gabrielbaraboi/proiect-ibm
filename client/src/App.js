05fffe8c4e6c1e7bc2fa7db49dbecdc08f946039
noobiedude <arpiarpad46@gmail.com>
2021-04-04 13:46:44 +0300

    set routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,27 @@
 import React, {useEffect} from 'react';
 
-import {Container,AppBar,Typography,Grow,Grid}  from '@material-ui/core';
-import {useDispatch} from 'react-redux';
+
 
 import {getPosts} from './actions/posts';
-import Posts from './components/Posts/Posts';
+
 
 import useStyles from './styles';
 
 const App = ()=>{
-    const classes = useStyles();
+    /*const classes = useStyles();
     const dispatch = useDispatch();
 
     useEffect(()=>{
         dispatch(getPosts());
-    },[dispatch]);
+    },[dispatch]);*/
     return (
-        <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">
-                    Oferte
-                </Typography>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <Router>
+        <div>
+          <Route exact path='/' component={ShowPosts} />
+          <Route path='/:id' component={ShowPost} />
+          <Route path='/add' component={AddPost}></Route>
+        </div>
+      </Router>
     );
 }
 export default App;
