1ab3aa629b11b3dd1d57909975805619400fbc23
Gabriel <gabrielbaraboi5@gmail.com>
2021-04-20 02:21:54 +0300

    register form depends on chosen role

--- client/src/components/Register.component.js
+++ client/src/components/Register.component.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useHistory } from 'react-router';
 import { register } from "../services/UserServices"
+import NavBar from "./NavBar.component"
+import styled from "styled-components";
+import { isUserData, getUserData } from "../services/localStorageManagment";
 
 const Field = React.forwardRef(({ label, type }, ref) => {
   return (
@@ -13,6 +16,9 @@ const Field = React.forwardRef(({ label, type }, ref) => {
 });
 
 const Form = ({ onSubmit }) => {
+
+  const [role, setRole] = useState('student');
+
   const emailRef = React.useRef();
   const firstnameRef = React.useRef();
   const lastnameRef = React.useRef();
@@ -20,18 +26,18 @@ const Form = ({ onSubmit }) => {
   const passwordRef = React.useRef();
   const roleRef = React.useRef();
   const dobRef = React.useRef();
+
   const handleSubmit = e => {
     e.preventDefault();
-    
     const data = {
       email: emailRef.current.value,
       password: passwordRef.current.value,
       role: roleRef.current.value,
     };
-    if(roleRef.current.value === 'company') {
+    if (roleRef.current.value === 'company') {
       data.companyName = companynameRef.current.value
     }
-    else if(roleRef.current.value === 'student') {
+    else if (roleRef.current.value === 'student') {
       data.firstName = firstnameRef.current.value
       data.lastName = lastnameRef.current.value
       data.DoB = dobRef.current.value
@@ -39,23 +45,43 @@ const Form = ({ onSubmit }) => {
     console.log(data)
     onSubmit(data);
   };
+
+
   return (
-    <form onSubmit={handleSubmit} >
-      <Field ref={emailRef} label="Email:" type="text" />
-      <Field ref={firstnameRef} label="First name:" type="text" />
-      <Field ref={lastnameRef} label="Last name:" type="text" />
-      <Field ref={companynameRef} label="Company name:" type="text" />
-      <Field ref={passwordRef} label="Password:" type="password" />
-      <Field ref={dobRef} label="Date of birth:" type="date" />
-      <select ref={roleRef}>
-        <option>Choose Role</option>
-        <option value='student'>Student</option>
-        <option value='company'>Company</option>
-      </select>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <main>
+      <NavBar></NavBar>
+      <Container>
+        <form onSubmit={handleSubmit} >
+          <label>Choose role:</label>
+          <input type="radio" ref={roleRef} value="student" name="role" id="student" onChange={e => setRole(e.target.value)} checked={role === "student"} />
+          <label for="student">Student</label>
+          <input type="radio" ref={roleRef} value="company" name="role" id="company" onChange={e => setRole(e.target.value)} checked={role === "company"} />
+          <label for="company">Company</label>
+          {role === "student" &&
+            <React.Fragment>
+              <Field ref={emailRef} label="Email:" type="text" />
+              <Field ref={firstnameRef} label="First name:" type="text" />
+              <Field ref={lastnameRef} label="Last name:" type="text" />
+              <Field ref={dobRef} label="Date of birth:" type="date" />
+              <Field ref={passwordRef} label="Password:" type="password" />
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </React.Fragment>
+          }
+          {role === "company" &&
+            <React.Fragment>
+              <Field ref={emailRef} label="Email:" type="text" />
+              <Field ref={companynameRef} label="Company name:" type="text" />
+              <Field ref={passwordRef} label="Password:" type="password" />
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </React.Fragment>
+          }
+        </form>
+      </Container>
+    </main>
   );
 };
 
@@ -63,11 +89,11 @@ const Form = ({ onSubmit }) => {
 export default () => {
   const handleSubmit = data => {
     register(data)
-    .then(() => {
-      history.push("/login");
-    })
-    .catch(err => { console.log(err); });
-};
+      .then(() => {
+        history.push("/login");
+      })
+      .catch(err => { console.log(err); });
+  };
 
   const history = useHistory();
   return (
@@ -76,3 +102,20 @@ export default () => {
     </div>
   )
 }
+
+const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  @media (min-width: 576px) {
+    max-width: 540px;
+  }
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+`;
