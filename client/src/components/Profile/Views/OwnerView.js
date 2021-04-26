adbffb5511831ec707fca4165d4b3035595df4d9
Alexandru Giupana <alexandru.giupana00@e-uvt.ro>
2021-04-26 12:39:34 +0300

    removed global editing, added a calendar when user wants to change its birthday

--- client/src/components/Profile/Views/OwnerView.js
+++ client/src/components/Profile/Views/OwnerView.js
@@ -3,18 +3,12 @@ import NavBar from "../../NavBar/NavBar.component";
 import { Link } from "react-router-dom";
 import Moment from "moment";
 import { ShowPostContainer, ImagePlace, AboutMe, AboutMeSmall, InformatiiGenerale, Continut } from '../ProfileStyledComponents'
-import EditProfile from "../EditProfile.component";
 
-import { EditDescription, EditName } from "../IndividualEditing";
+import { EditDescription, EditName, EditDoB } from "../IndividualEditing";
 
 
 export const ShowProfileToOwner = ({ postData, deleteThisUser }) => {
     
-    const [EditMode, setEditMode] = useState(false);
-    const toggleEdit = () => {
-        setEditMode(false);
-    }
-
     const [EditAboutMe, setEditAboutMe] = useState(false);
     const toggleEditAboutMe = () => {
         setEditAboutMe(false);
@@ -22,6 +16,10 @@ export const ShowProfileToOwner = ({ postData, deleteThisUser }) => {
     const [EditTheName, setEditName] = useState(false);
     const toggleEditName = () => {
         setEditName(false);
+    }
+    const [EditTheDoB, setEditDoB] = useState(false);
+    const toggleEditDoB = () => {
+        setEditDoB(false);
     }
 
 
@@ -43,36 +41,35 @@ export const ShowProfileToOwner = ({ postData, deleteThisUser }) => {
                     
                     (<EditDescription toggleEditAboutMe={toggleEditAboutMe} connectedUser = {postData} small={false}></EditDescription>)
                 }
-
-                {EditMode ? ( 
-                    <EditProfile toggleEdit={toggleEdit} connectedUser = {postData}></EditProfile>
-                ) : (
-                    <div>
-                        <Continut>
-                        {!EditTheName ?
-                            (<>
-                                {postData?.detalii?.companyName} {postData?.detalii?.firstName} {postData?.detalii?.lastName}
-                                <button onClick={() => setEditName(true)}>Edit</button><br></br>
-                            </>)
-                            
-                            : 
-                            (<EditName toggleEditName={toggleEditName} connectedUser = {postData} small={false}></EditName>
-                             )
-                        }
-                        <button onClick={() => setEditMode(true)}>Global Editing</button>
-                        <button onClick={() => deleteThisUser()}>Delete your account</button>
-                        {/* (<EditDescription toggleEditAboutMe={toggleEditAboutMe} connectedUser = {postData} small={false}></EditDescription>) */}
-                    </Continut>
+             
+                <Continut>
+                    {!EditTheName ?
+                        (<>
+                            {postData?.detalii?.companyName} {postData?.detalii?.firstName} {postData?.detalii?.lastName}
+                            <button onClick={() => setEditName(true)}>Edit</button><br></br>
+                        </>)
                         
-                    </div>
-                )
-                }
-            
+                        : 
+                        (<EditName toggleEditName={toggleEditName} connectedUser = {postData} small={false}></EditName>
+                            )
+                    }
+                    <button onClick={() => deleteThisUser()}>Delete your account</button>
+                </Continut>
+
             </InformatiiGenerale>
             
             <InformatiiGenerale>
                 {postData?.detalii?.email} <br/>
-                {postData?.detalii?.DoB ? Moment(postData?.detalii?.DoB).format('DD-MM-YYYY') : (<></>)} 
+                {(postData?.detalii?.role == "student") ?
+                    (!EditTheDoB ?
+                        (<>
+                            {postData?.detalii?.DoB ? Moment(postData?.detalii?.DoB).format('DD-MM-YYYY') : (<></>)}
+                            <button onClick={() => setEditDoB(true)}>Edit</button>
+                            
+                        </>)
+                            : (<EditDoB toggleEditDoB={toggleEditDoB} connectedUser = {postData}> </EditDoB>)
+                    ) : (<></>)
+                } 
             </InformatiiGenerale>
 
             <AboutMeSmall>
